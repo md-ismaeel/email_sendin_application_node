605c9377eb90dd6f059ba25d27cdff40ae34dc59
Md Ismail <mdismaeelkhan345@gmail.com>
2024-06-13 15:23:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require('path')
-const cors = require('cors')
-
+const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
 const routers = require("./Routes/routers");
 const { mailRoute } = routers;
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const PORT = 10000;
 
@@ -14,22 +14,30 @@ const PORT = 10000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const staticDirectory = path.join(__dirname, 'front-end');
+console.log(`Serving static files from: ${staticDirectory}`);
+
+// Log directory contents for verification
+fs.readdir(staticDirectory, (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    console.log('Directory contents:', files);
+});
+
 app.use(express.static(staticDirectory));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(staticDirectory, "index.html"));
-})
+});
 
-
-app.use(mailRoute)
+app.use(mailRoute);
 
 app.use('/*', (req, res) => {
     res.status(404).json({
         success: false,
         message: 'Path not found'
-    })
-})
+    });
+});
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
